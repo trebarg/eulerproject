843e5ba6cf47cd325648979c542159854cda90a2
trebarg <trebarg@thegraberts.com>
2016-07-20 10:28:48 -0400

    fixed "adundant"

--- euler problem 17 (Number letter counts).py
+++ euler problem 17 (Number letter counts).py
@@ -1,3 +1,6 @@
+import math
+import time
+start_time = time.time()
 eulerNumber = 1000
 letterCounts = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety", 100: "onehundred", 200: "twohundred", 300: "threehundred", 400: "fourhundred", 500: "fivehundred", 600: "sixhundred", 700: "sevenhundred", 800: "eighthundred", 900: "ninehundred", 1000: "onethousand" }
 i = 1
@@ -85,3 +88,4 @@ while i <= eulerNumber:
 ##print(charCount)
 print(bigString)
 print(len(bigString))
+print(time.time() - start_time, "seconds")
