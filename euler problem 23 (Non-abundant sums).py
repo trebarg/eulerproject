6f368f5e9bc4a7fffaffa1cfef6e7f0d347c7c72
trebarg <trebarg@thegraberts.com>
2016-07-21 13:06:24 -0400

    fixed "adundant"

--- euler problem 23 (Non-abundant sums).py
+++ euler problem 23 (Non-abundant sums).py
@@ -3,8 +3,12 @@ import time
 start_time = time.time()
 
 bound = 28123
-i = 1
 
+
+i = 1
+perfectNumbers = list()
+abundantNumbers = list()
+deficientNumbers = list()
 
 while i <= bound:
     currentAmicableSum = 0
@@ -19,10 +23,13 @@ while i <= bound:
         numberType = ""
         if i == currentAmicableSum:
             numberType = "Perfect"
+            perfectNumbers.append(i)
         elif i < currentAmicableSum:
             numberType = "Abundant"
+            abundantNumbers.append(i)
         elif i > currentAmicableSum:
             numberType = "Deficient"
+            deficientNumbers.append(i)
         print str(i) + " " + str(0) + " " + numberType
     else:
         while x <= halfEye:
@@ -34,12 +41,36 @@ while i <= bound:
             x += 1
         numberType = ""
         if i == currentAmicableSum:
-                numberType = "Perfect"
+            numberType = "Perfect"
+            perfectNumbers.append(i)
         elif i < currentAmicableSum:
             numberType = "Abundant"
+            abundantNumbers.append(i)
         elif i > currentAmicableSum:
             numberType = "Deficient"
+            deficientNumbers.append(i)
         print str(i) + " " + str(currentAmicableSum)+" "+numberType
     i += 1
 
+print "Perfect: "+str(len(perfectNumbers))
+print "Abundant: "+str(len(abundantNumbers))
+print "Deficient: "+str(len(deficientNumbers))
+
+abundantSums = list()
+for number in abundantNumbers:
+    for innerNumber in abundantNumbers:
+        if (number+innerNumber) < bound:
+            abundantSums.append(number + innerNumber)
+print len(abundantSums)
+uniqueNumbers = list(set(abundantSums))
+print len(uniqueNumbers)
+finalAnswer = 0
+
+i = 1
+
+while i <= bound:
+    if i not in uniqueNumbers:
+        finalAnswer += i
+    i += 1
+print "FinalAnswer: "+str(finalAnswer)
 print(time.time() - start_time, "seconds")
